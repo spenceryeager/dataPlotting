d7f0b8111234d90212eea0c78279111f39ee8e6c
Spencer Yeager <yeager.spencer@gmail.com>
2023-11-28 17:55:20 -0700

    starting script for printing horiba pl data

---
+++ horiba_nanofab_plotting.py
@@ -0,0 +1,22 @@
+# this is for plotting the PL from the Horiba in the nanofab.
+# Exporting spectra as a text file gives a pretty 
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from matplotlib import rcParams
+rcParams['font.family'] = 'sans-serif'
+rcParams['font.sans-serif'] = ['Arial']
+rcParams['font.weight'] = 'bold'
+rcParams['axes.labelweight'] = 'bold'
+rcParams['savefig.dpi'] = 300
+
+
+def main():
+    print("hi")
+
+
+if __name__ == "__main__":
+    main()
+    
